032cfa9b6a487b30ce4013704ee160c976f12a99
vibend <zane.korelic@gmail.com>
2015-10-13 11:25:41 +0200

    Update twitch_emoticons.meta.js

--- build/twitch_emoticons.meta.js
+++ build/twitch_emoticons.meta.js
@@ -5,6 +5,6 @@
 // @include     https://www.irccloud.com/*
 // @version     2.0.0
 // @grant       none
-// @updateURL   https://github.com/dogancelik/irccloud-twitch-emoticons/raw/master/build/twitch_emoticons.meta.js
-// @downloadURL https://github.com/dogancelik/irccloud-twitch-emoticons/raw/master/build/twitch_emoticons.user.js
+// @updateURL   https://github.com/vibend/irccloud-twitch-emoticons/raw/master/build/twitch_emoticons.meta.js
+// @downloadURL https://github.com/vibend/irccloud-twitch-emoticons/raw/master/build/twitch_emoticons.user.js
 // ==/UserScript==

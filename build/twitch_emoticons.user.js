460648b23973c07ca8c45a6552ca898f6d28d350
vibend <zane.korelic@gmail.com>
2015-10-13 11:27:21 +0200

    Update twitch_emoticons.user.js

--- build/twitch_emoticons.user.js
+++ build/twitch_emoticons.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name        Twitch Emoticons
-// @namespace   dogancelik.com
+// @namespace   vibend
 // @description Enables Twitch emoticons in IRCCloud
 // @include     https://www.irccloud.com/*
 // @version     2.0.0
 // @grant       none
-// @updateURL   https://github.com/dogancelik/irccloud-twitch-emoticons/raw/master/build/twitch_emoticons.meta.js
-// @downloadURL https://github.com/dogancelik/irccloud-twitch-emoticons/raw/master/build/twitch_emoticons.user.js
+// @updateURL   https://github.com/vibend/irccloud-twitch-emoticons/raw/master/build/twitch_emoticons.meta.js
+// @downloadURL https://github.com/vibend/irccloud-twitch-emoticons/raw/master/build/twitch_emoticons.user.js
 // ==/UserScript==
 
 //jslint jquery: true
